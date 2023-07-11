0884b1823e224cc6ca98a73bb9ea2758a66b4e4a
박상훈 <psh950@naver.com>
2023-07-11 20:48:18 +0900

    dfs 하던거

---
+++ 3.dfsbps/음료수얼려먹기.py
@@ -0,0 +1,25 @@
+
+#
+M = 4
+N = 5
+visited = [[False] * M] * N
+graph = [
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+]
+
+def dfs(i, j):
+
+
+def solution(N, M, lst=[], visited=[]):
+    stack = []
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 0:
+                stack.append()
+    return 0
+
+print(solution(N, M, graph))
+
