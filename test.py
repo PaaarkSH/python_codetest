a8f7d971572a751c7fb0c1289c545812265876cf
박상훈 <psh950@naver.com>
2023-07-20 11:55:21 +0900

    dp 피보나치 수정

--- test.py
+++ test.py
@@ -1,47 +1,16 @@
-from datetime import datetime
+def solution(A):
+    prev, diff = -1, 10 ** 9
+    A.sort()
+    for a in A:
+        if prev != -1:
+            diff = min(diff, abs(a - prev))
+        prev = a
+    return diff if len(set(A)) != 1 else 0
 
 
-def is_leap_year(year):
-    # 윤년 여부를 판단하는 함수
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+print(solution([1, 1]))
+print(solution([1, 1, 1, 5]))
+print(solution([10, 5]))
+print(solution([9, 11, 7]))
+print(solution([7, 13, 10]))  # 7 10 13
 
-def calculate_date_difference(start_date, end_date):
-    start = datetime.strptime(start_date, "%Y-%m-%d")
-    end = datetime.strptime(end_date, "%Y-%m-%d")
-
-    # 날짜 차이 계산
-    delta = end - start
-
-    # 총 일 수 차이 계산
-    total_days = delta.days
-
-    # 연도와 월을 기준으로 달 수 차이 계산
-    start_year = start.year
-    start_month = start.month
-
-    end_year = end.year
-    end_month = end.month
-
-    year_diff = end_year - start_year
-    month_diff = end_month - start_month
-
-    # 윤년 고려하여 달 수 차이 조정
-    if month_diff < 0:
-        year_diff -= 1
-        month_diff += 12
-
-    # 윤년 고려하여 일 수 차이 조정
-    leap_years = sum(1 for year in range(start_year, end_year + 1) if is_leap_year(year))
-
-    total_months = year_diff * 12 + month_diff
-    adjusted_total_days = total_days - leap_years
-
-    return total_months, adjusted_total_days
-
-# 두 날짜를 입력받아 달 수 차이와 일 수 차이 출력
-start_date = '2023-01-01'
-end_date = '2023-02-08'
-
-month_diff, day_diff = calculate_date_difference(start_date, end_date)
-print("달 수 차이:", month_diff)
-print("일 수 차이:", day_diff)
